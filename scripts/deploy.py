371d3febd52ca66bc9264f53d5fbdf6f3c815c57
JMario1 <jdonmarie@gmail.com>
2022-03-09 13:07:53 +0100

    first commit

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,14 +1,6 @@
 import json
 from brownie import accounts, HelloWorld
 
-def get_abi():
-    with open("./build/contracts/HelloWorld.json", "r") as file:
-        file1 = json.load(file)
-        with open("./abi", "w") as file2:
-            json.dump(file1["abi"], file2)
-        with open("./bin", "w") as file3:
-            json.dump(file1["bytecode"], file3)    
-            json.dump(file1["deployedBytecode"], file3) 
 
 def deploy_hello_world():
     account = accounts.load("rinkeby1")
@@ -19,5 +11,4 @@ def deploy_hello_world():
     print(hello_world.Greeting())
 
 def main():
-    get_abi()
     deploy_hello_world()
